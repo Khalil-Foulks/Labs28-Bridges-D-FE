01ce7dabce9fb9bd0e95be9850403aa530421d73
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-08-12 18:19:11 -0500

    trying to add the file i thought i previously added

--- src/components/pages/Main/Main.js
+++ src/components/pages/Main/Main.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Layout } from 'antd';
+import {
+  FacebookFilled,
+  TwitterOutlined,
+  InstagramOutlined,
+  LinkedinFilled,
+} from '@ant-design/icons';
 import './Main.css';
 
 //destructing components out of Layout component
@@ -30,7 +36,49 @@ const Main = () => {
             culpa qui officia deserunt mollit anim id est laborum.
           </Content>
         </Layout>
-        <Footer className="footer">footer</Footer>
+        <Footer className="footer">
+          <div className="ftrWebsiteLinks">
+            <a href="https://bridgestoprosperity.org/">Home</a> <br></br>
+            <a href="https://bridgestoprosperity.org/join-us/">Join Us</a>
+            <br></br>
+            <a href="https://bridgestoprosperity.org/faqs/">FAQs</a>
+            <br></br>
+            <a href="https://bridgestoprosperity.org/contact//">Contact Us</a>
+          </div>
+          <div className="ftrSocialLinks">
+            <FacebookFilled style={{ fontSize: '20px' }} />
+            <a href="https://www.facebook.com/BridgestoProsperity/">
+              {' '}
+              - @BridgestoProsperity
+            </a>
+            <br></br>
+            <TwitterOutlined style={{ fontSize: '20px' }} />
+            <a href="https://twitter.com/b2p"> - @B2P</a>
+            <br></br>
+            <InstagramOutlined style={{ fontSize: '20px' }} />
+            <a href="https://www.instagram.com/bridgestoprosperity/">
+              {' '}
+              - @bridgestoprosperity
+            </a>
+            <br></br>
+            <LinkedinFilled style={{ fontSize: '20px' }} />
+            <a href="https://www.linkedin.com/company/bridges-to-prosperity">
+              {' '}
+              - Bridges to Prosperity
+            </a>
+          </div>
+          <div className="ftrInfo">
+            <img
+              className="logoFtr"
+              src="B2P_Symbol_White.svg"
+              alt="B2P Logo"
+            />
+            <h5>
+              3858 Walnut St., Suite 219 Denver, Colorado 80205 EIN: 54-2031102
+            </h5>
+            <h6>2018 © Bridges to Prosperity. All rights reserved.</h6>
+          </div>
+        </Footer>
       </Layout>
     </div>
   );
