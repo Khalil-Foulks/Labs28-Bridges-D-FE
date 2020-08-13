8be8c954fb993af9a27a8df10bb188b09e8c01f4
Jason Hemann <43769789+JasonRHemann@users.noreply.github.com>
2020-08-13 16:49:41 -0500

    Feature--Added popups when clicking on bridges

--- src/components/pages/Map/Map.js
+++ src/components/pages/Map/Map.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import * as bridgeData from './bridges.json';
 
@@ -12,7 +12,17 @@ const Map = () => {
     height: '79.3vh',
     zoom: 7.4,
   });
+
   const [selectedBridge, setSelectedBridge] = useState(null);
+
+  useEffect(() => {
+    const listener = e => {
+      if (e.key === 'Escape') {
+        setSelectedBridge(null);
+      }
+    };
+    window.addEventListener('keydown', listener);
+  }, []);
 
   return (
     <ReactMapGL
@@ -25,12 +35,14 @@ const Map = () => {
         setViewport(viewport);
       }}
     >
+      {/* Maps through all the data in bridges.json grabbing lat and lon to display markers */}
       {bridgeData.features.map(bridge => (
         <Marker
           key={bridge.properties.bridge_id}
           latitude={bridge.geometry.coordinates[0]}
           longitude={bridge.geometry.coordinates[1]}
         >
+          {/* image used to display point on map */}
           <img
             className="marker-btn"
             src="bridge.svg"
@@ -47,9 +59,17 @@ const Map = () => {
         <Popup
           latitude={selectedBridge.geometry.coordinates[0]}
           longitude={selectedBridge.geometry.coordinates[1]}
+          //Closes popup when X is clicked by resetting state to null
+          onClose={() => {
+            setSelectedBridge(null);
+          }}
         >
+          {/* Div to display what data is wanted in the popup*/}
           <div>
-            <h4> </h4>
+            <h4>{selectedBridge.properties.BridgeSiteName}</h4>
+            <p>Status: {selectedBridge.properties.ProjectStage}</p>
+            <p>Type: {selectedBridge.properties.BridgeType}</p>
+            <p>Span: {selectedBridge.properties.Span} meters</p>
           </div>
         </Popup>
       ) : null}
